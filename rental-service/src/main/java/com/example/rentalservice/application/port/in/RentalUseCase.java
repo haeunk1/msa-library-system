35c82808027897c034d0f5f7c741c0b36c05108a
Haeun Kim <53562331+haeunk1@users.noreply.github.com>
2025-06-25 12:16:56 +0900

    feat: 서비스 생성

--- rental-service/src/main/java/com/example/rentalservice/application/port/in/RentalUseCase.java
+++ rental-service/src/main/java/com/example/rentalservice/application/port/in/RentalUseCase.java
@@ -1,5 +1,29 @@
 package com.example.rentalservice.application.port.in;
 
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+
+import com.example.rentalservice.domain.Rental;
+
 public interface RentalUseCase {
+    // Rental save(Rental rental);
+    // Page<Rental> findRentalAll(Pageable pageable);
+    // Optional<Rental> findRentalOne(Long id);
+    // void delete(Long id);
+
+    /* 대출 */
+    Rental rentBook(Long memberId, Long bookId);
+    /* 반납 */
+    Rental returnBook(Long memberId, Long bookId);
     
+    /* 연체 도서 반납 */
+    Rental returnOverdueBook(Long memberId, Long bookId);
+    /* 연체 상태로 변경 */
+    Long beOverdueBook(Long rentalId);
+    /* 연체 상태 해제하기 */
+    Rental releaseOverdue(Long memberId);
+    /* Rental 조회 */
+    Optional<Rental> findRentalByMember(Long memberId);
 }
--- rental-service/src/main/java/com/example/rentalservice/application/service/RentalService.java
+++ rental-service/src/main/java/com/example/rentalservice/application/service/RentalService.java
@@ -1,5 +1,58 @@
 package com.example.rentalservice.application.service;
 
-public class RentalService {
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.rentalservice.application.port.in.RentalUseCase;
+import com.example.rentalservice.application.port.out.RentalRepository;
+import com.example.rentalservice.domain.Rental;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RentalService implements RentalUseCase{
+
+    private final RentalRepository rentalRepository;
+
+    /* 도서 대출하기기 */
+    @Override
+    public Rental rentBook(Long memberId, Long bookId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rentBook'");
+    }
+
+    @Override
+    public Rental returnBook(Long memberId, Long bookId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'returnBook'");
+    }
+
+    @Override
+    public Rental returnOverdueBook(Long memberId, Long bookId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'returnOverdueBook'");
+    }
+
+    @Override
+    public Long beOverdueBook(Long rentalId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'beOverdueBook'");
+    }
+
+    @Override
+    public Rental releaseOverdue(Long memberId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'releaseOverdue'");
+    }
+
+    @Override
+    public Optional<Rental> findRentalByMember(Long memberId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findRentalByMember'");
+    }
     
 }
